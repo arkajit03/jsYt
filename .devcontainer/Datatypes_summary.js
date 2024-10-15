fab6b6c668917cd1a40e0a4796a4a0bd904c4c7a
arkajit03 <134793902+arkajit03@users.noreply.github.com>
2024-10-15 11:54:23 +0000

    data type done

---
+++ .devcontainer/Datatypes_summary.js
@@ -0,0 +1,9 @@
+// primitive 
+
+// 7 type : string,number,Boolean,null,undefined,symbol,Bigint
+//this all are call by value 
+
+
+// Non primitive
+//Array, Objects, Functions
+//JavaScript is a dynamic language its mean we don't have to define  variable data type
---
+++ .devcontainer/basics/01_variable.js
@@ -0,0 +1,4 @@
+const accountId=144553
+let accountEmail="arkajit@gmail"
+city="Kolkata"
+console.table([accountId, accountEmail, city])
