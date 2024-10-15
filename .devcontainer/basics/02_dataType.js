25676a521235923a363a3c25e36f9f73d4d112f0
arkajit03 <134793902+arkajit03@users.noreply.github.com>
2024-10-15 08:21:13 +0000

    data type

---
+++ .devcontainer/basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict";// js will treat all code as newer version
+//alter(6+6) we can't do it in nodejs,go with browser
+// code readability shud be good 
+//let is a data type 
+//if we write  let with out "" then it is a int and if we write with "" it is a string
+let name= "arkajit"
+let age = 21
+//if true or fasle give than it is a boolean value;
+// number =>2 ^ 53
+// bigint for big nuber
+// string
+// null it an standalon value its mean the value is undefine but the variable exist;
+let hight=null;
+// undefined => its mean variable created but no value assined
+let state;
+//symbol=> too creat uniqueness in  code
+
+//object 
+console.log(typeof null);//object type
