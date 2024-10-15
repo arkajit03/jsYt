7c64b5eb3c5fd6decede56962d965707f8bac3c5
arkajit03 <134793902+arkajit03@users.noreply.github.com>
2024-10-15 08:49:45 +0000

    data type conversion

---
+++ .devcontainer/basics/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score= 33
+// we will work on backend so we will get value from frontend now this 33 can be number string anything so 
+let score1="44"
+console.log(typeof score1);
+// to convert thuis string in int
+let scoreA=Number(score1);
+console.log(typeof scoreA);
+//now if we get 44abc and convert it in a number then we will  get data type as number but it will be NaN
+let islog=1
+let boolean=Boolean(islog)
+console.log(boolean);
+//we will get output as  true becaue 1 is for true and 0 is for false
+// so in summary to conver we will capital the first latter of the datatype of lthe variable like String,Number,Boolean
